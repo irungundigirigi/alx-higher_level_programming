339ed19a9eeb19723eaa4aeec41e902536eec260
irungundigirigi <irungundigirigi@gmail.com>
2023-08-07 23:26:35 +0300

    Update 1-run_inline

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for cycle in a linked list
+ * @list: linked list
+ *
+ * Return: linked list has cycle 1 else 0
+ */
+
+int check_cycle(list_t *list)
+{
+	listint_t *tortoise = list;
+	listint_t *hare = list;
+
+	if (!list)
+		return (0);
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
+	}
+
+	return (0);
+}
