df035123b1a01c469be8200c93daf36d6b699fb9
irungundigirigi <irungundigirigi@gmail.com>
2023-08-29 19:25:36 +0300

    Add tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,11 @@
 """Defines a square"""
 
 class Square:
+    """Represents a square"""
+
     def __init__(self, size):
+        """Initializing square class
+        Args: size -represent size of square defined
+        """
+
         self.__size = size
