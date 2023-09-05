a6c5b970aab064225efb0382341847f9ca643fae
irungundigirigi <irungundigirigi@gmail.com>
2023-09-06 00:08:50 +0300

    Add tasks

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    """Suite test for max_integer function"""
+    """Test for max_integer function"""
 
     def test_max_integer(self):
         self.assertEqual(max_integer([5, -2, 100, 3]), 100)
@@ -21,9 +21,6 @@ class TestMaxInteger(unittest.TestCase):
     def test_float_numbers(self):
         self.assertEqual(max_integer([50, 51, 50, 49]), 51)
 
-    def test_max_operated_integer(self):
-        self.assertEqual(max_integer([-3, -5 * -5, 12, -1]), 25)
-
     def test_neg_numbers(self):
         self.assertEqual(max_integer([-10, -5, -2, -1]), -1)
 
@@ -32,6 +29,11 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_zero_number(self):
         self.assertEqual(max_integer([0, 0]), 0)
+
+    def test_max_operated_integer(self):
+        self.assertEqual(max_integer([-3, -5 * -5, 12, -1]), 25)
+
+
 
     def test_big_list(self):
         self.assertEqual(max_integer([
