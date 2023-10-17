359a042fb64facc57475719a956cbd373813e559
irungundigirigi <irungundigirigi@gmail.com>
2023-10-17 21:31:02 +0300

    add sql tasks

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- lists all rows , display score and name ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- create a table first_table in current db
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
