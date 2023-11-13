377c189c8e673ce94b4b85adfe77c76e1a8cfd3d
irungundigirigi <irungundigirigi@gmail.com>
2023-11-13 12:41:03 +0300

    update

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Prints "JavaScript is amazing".
+
+const myVar = 'Javascript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Prints "JavaScript is amazing".
+
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myVar);
