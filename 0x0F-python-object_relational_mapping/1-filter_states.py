44830ba9e1d553b151a53bc98060a2df57140d02
irungundigirigi <irungundigirigi@gmail.com>
2023-11-18 13:33:05 +0300

    Add task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Lists all states with a name starting with uppercase N
+# Username, password, and dabase names given as arguments
+# MySQLdb is an interface for connecting MySQL database server from Python
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host='localhost',
+            port=3306
+            )
+    cur = db.cursor()
+    cur.execute(""" SELECT id, name
+                    FROM states
+                    WHERE name LIKE 'N%'
+                    ORDER BY id ASC;""")
+
+    # Fetch all rows of the query
+    states = cur.fetchall()
+
+    for state in states:
+        if (state[1][0] == 'N'):
+            print(state)
+
+    cur.close()
+    db.close()
