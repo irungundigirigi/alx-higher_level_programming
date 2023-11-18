6cd58c1b8606bdf9a10c2192188e7803318b767a
irungundigirigi <irungundigirigi@gmail.com>
2023-11-18 15:19:28 +0300

    update code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,16 +17,11 @@ if __name__ == "__main__":
             port=3306
             )
     cur = db.cursor()
-    cur.execute(""" SELECT id, name
-                    FROM states
-                    WHERE name='{}'
-                    ORDER BY id ASC;""".format(sys.argv[4]));
+    cur.execute(""" SELECT * FROM states
+                    WHERE name LIKE BINARY '{}'
+                    ORDER BY states.id ASC;""".format(sys.argv[4]));
 
-    # Fetch all rows of the query
     states = cur.fetchall()
 
     for state in states:
             print(state)
-
-    cur.close()
-    db.close()
