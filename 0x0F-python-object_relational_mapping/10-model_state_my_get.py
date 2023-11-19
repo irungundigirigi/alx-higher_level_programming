1931045b8ea7d62564b61d5f7ab6751cb56fd66c
irungundigirigi <irungundigirigi@gmail.com>
2023-11-19 14:42:08 +0300

    update tasks

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This script lists all State objects from `hbtn_0e_6_usa` db
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+        Get states from database
+    """
+    
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+
+    if state is None:
+        print('Not found')
+    else:
+        print('{0}'.format(state.id))
