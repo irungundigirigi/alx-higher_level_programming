5e9a1738d5c0ebeaf0f5a34913883083123a8a35
irungundigirigi <irungundigirigi@gmail.com>
2023-12-02 00:39:12 +0300

    add tasks

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Sends a POST request with email body
+Usage: ./2-post_email.py <URL> <email>
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email).encode("ascii")
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
+
+
