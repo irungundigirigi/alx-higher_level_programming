ba71da20c2da8ae1271d9f56a8269525dd9d702f
irungundigirigi <irungundigirigi@gmail.com>
2023-12-02 20:37:10 +0300

    make code pycodestyle compliant

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,8 @@ import sys
 import urllib.error
 import urllib.request
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as res:
