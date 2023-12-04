d95fdefd4c811f1c06d6e598e75af3eee850ab68
irungundigirigi <irungundigirigi@gmail.com>
2023-12-04 10:51:24 +0300

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,5 +16,4 @@ if __name__ == '__main__':
                 commits[i].get("sha"),
                 commits[i].get("commit").get("author").get("name")))
     except IndexError:
-        pass
-    
+        pass    
