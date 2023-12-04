2ba989666bf693ce9a022798d1745ac5b467cc9e
irungundigirigi <irungundigirigi@gmail.com>
2023-12-04 10:48:36 +0300

    update

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+if __name__ == '__main__':
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    res = requests.get(url)
+    commits = res.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
+    
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Fetches url using package requests """
+""" Fetches url using package requests """i
 import requests
 
-
 if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
