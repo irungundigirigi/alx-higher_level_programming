a54f33e98bb38d17dcc3f89570301243079df492
irungundigirigi <irungundigirigi@gmail.com>
2024-01-03 19:27:14 +0300

    change directories

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`The response body has been successfully stored in ${filePath}`);
+      }
+    });
+  }
+});
+
